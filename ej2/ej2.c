9e3ef7f67e892c60d490cc1d11e01e90052f5cc2
Santiago Libonati <santiago.libonati@gmail.com>
2024-09-24 13:57:57 -0300

    Avances en la resolucion del ejercicio 2

--- ej2/ej2.c
+++ ej2/ej2.c
@@ -38,16 +38,48 @@ void usage(char* progName){
   printf("de velocidad desde 40%% y 50%%\n\n");
 }
 
+double resolventeSimplificada(int sumar, const double velocidad, const double dosA, const double cuatroAC){
+  double dividendo;
+  if (sumar) {
+    dividendo = -velocidad + sqrt((velocidad*velocidad) - cuatroAC);
+  } else {
+    dividendo = -velocidad - sqrt((velocidad*velocidad) - cuatroAC);
+  }
+  return dividendo / dosA;
+}
+
+double max(const double a, const double b){
+  return a > b ? a : b;
+}
+
 void test(double velocidad_inicial, double angulo, double perdida){
-  double velocidad_horizontal, velocidad_vertical;
-  double rad_angulo;
+  double posicion_inicial = 0.15;
   char *resultadoA="Sin resolver", *resultadoB="Sin resolver", *resultadoC="Sin resolver";
 
   printf("TEST: velocidad inicial=%.2lf m/s; angulo=%.2lf grados; perdida=%.2lf%%\n", velocidad_inicial, angulo, perdida);
 
-  rad_angulo = (angulo * M_PI) / 180.0;
-  velocidad_horizontal = velocidad_inicial * cos(rad_angulo);
-  velocidad_vertical = velocidad_inicial * sin(rad_angulo);
+  const double angulo_parabola = 90 - angulo, rad_angulo = (angulo_parabola * M_PI) / 180.0;
+
+  // cuatroAC = 2 * 0.15 * g
+  // dosA = 2 * -1/2 g = -g
+  const double coseno = cos(rad_angulo), seno = sin(rad_angulo), dosA = -9.8, cuatroAC = 2.94, gravedad = 9.8;
+
+  const double velocidad_horizontal = velocidad_inicial * coseno;
+  double velocidad_vertical = velocidad_inicial * seno;
+
+  double t1, t2, siguiente_posicion;
+  for (int escalon = 0; escalon < 8; escalon++) {
+    velocidad_vertical = velocidad_vertical * perdida/100;
+
+    // obtenemos el tiempo que falta para que rebote
+    t1 = resolventeSimplificada(1, velocidad_vertical, dosA, cuatroAC);
+    t2 = resolventeSimplificada(0, velocidad_vertical, dosA, cuatroAC);
+
+    double t = max(t1, t2);
+
+    // obtenemos la siguiente posicion
+    siguiente_posicion = posicion_inicial + velocidad_horizontal*t;
+  }
   
   
 
@@ -75,6 +107,10 @@ int main(int argc, char *argv[])
     case 4:
       velocidad_inicial = get_double(argv[1]);
       angulo = get_double(argv[2]);
+      if (angulo > 90 || angulo < 0){
+        fprintf(stderr, "El angulo en que se suelta no puede ser mayor a 90 grados o menor a 0 grados\n");
+        exit(EXIT_FAILURE);
+      }
       perdida = get_double(argv[3]);
 
       test(velocidad_inicial, angulo, perdida);
