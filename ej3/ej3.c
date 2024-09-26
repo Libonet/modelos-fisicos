5854fc829c8e2e822201cee116bb638699ccb8ae
tomasmaiza <tomimaiza@gmail.com>
2024-09-26 20:34:56 -0300

    Modifico la resolución del 3a

--- ej3/ej3.c
+++ ej3/ej3.c
@@ -2,17 +2,20 @@
 #include <stdio.h>
 #include <math.h>
 
+#define FT_TO_M 0.3048 // valor de 1 pie en metros
+#define MI_TO_M 1609.34 // valor de 1 milla en metros
+#define H_TO_S 3600 // valor de 1 hora en segundos
+
 void usage();
 void apartado_a1();
+void apartado_a2();
 
 int main(int argc, char *argv[]) {
   if (argc == 2) {
     int opc = atoi(argv[1]);
     switch (opc) {
       case 1: apartado_a1(); break;
-      case 2: break;
-      case 3: break;
-      case 4: break;
+      case 2: apartado_a2(); break;
       default: usage(); break;
     }
   }
@@ -25,16 +28,21 @@ void usage(){
   printf("OPCIONES:\n");
   printf("1: la rampa es recta y la aceleración permanece constante\n");
   printf("2: la rampa es recta y la aceleración varía linealmente\n");
-  printf("3: la rampa es curva y la aceleración permanece constante\n");
-  printf("4: la rampa es curva y la aceleración varía linealmente\n");
 }
 
 void apartado_a1() {
-  // tomamos t_0 = 0s, x_0 = 0 ft y la aceleración constante tal que -10 ft/s² <= a(t) <= 0 ft/s²
-  // queremos calcular el tiempo final mínimo t_f y la distancia recorrida por el automóvil sobre la rampa.
-  float a = -10; // ft/s²
-  float t_f = -88 / a; // minimizamos t_f cuando a = -10 ft/s²
-  printf("El tiempo mínimo requerido para que el automóvil quede en reposo es %.2f s\n", t_f);
-  float x_f = -3872 / a;
-  printf("La distancia recorrida sobre la rampa es %.2f ft\n", x_f);
+  float a = -10; // aceleración en ft/s^2
+  a = a * FT_TO_M; // aceleración en m/s^2
+  float v_0 = 60; // velocidad inicial en mi/h
+  v_0 = v_0 * MI_TO_M / H_TO_S; // velocidad inicial en m/s
+  
+  float t = -v_0/a; // tiempo final en s
+  printf("El tiempo requerido para que el automóvil quede en reposo con la aceleración constante dada es %.2f s\n", t);
+
+  float d = - v_0 * v_0 / (2 * a); // distancia recorrida en m, como x_0 = 0m, d es igual al valor de x(t), dado por esta ecuación.
+  printf("La distancia recorrida sobre la rampa es %.2f m\n", d);
+}
+
+void apartado_a2() {
+
 }
