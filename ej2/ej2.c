e097a1f77562a96b4e380719bb77093c1705be5f
Santiago Libonati <santiago.libonati@gmail.com>
2024-09-29 15:35:11 -0300

    Parece que anda?
    No esta chequeado para nada. Por favor ayuda 🙊

--- ej2/ej2.c
+++ ej2/ej2.c
@@ -42,58 +42,110 @@ double max(const double a, const double b){
   return (a > b) ? a : b;
 }
 
-double resolventeSimplificada(const double velocidad, const double dosA, const double cuatroAC){
-  double dividendo;
-  dividendo = sqrt((velocidad*velocidad) + cuatroAC) - velocidad;
-  if (dividendo > 0) {
-    dividendo = -sqrt((velocidad*velocidad) + cuatroAC) - velocidad;
-  }
-  return dividendo / dosA;
+// y(t) = v0*cos(ang)*t - 1/2 g t^2
+// 0 = -g/2 * t^2 + v0*cos(ang) * t - y
+// t1,t2
+// tiempo = max(t1,t2)
+double resolver_tiempo(double const altura, double const velocidad){
+  double tiempo = 0;
+  double const a = 4.905, dosA = -9.81, cuatroAC = 4*(-a)*(-altura);
+
+  double t1 = (-velocidad + sqrt(pow(velocidad, 2) - cuatroAC)) / dosA;
+  double t2 = (-velocidad - sqrt(pow(velocidad, 2) - cuatroAC)) / dosA;
+
+  tiempo = max(t1,t2);
+
+  return tiempo;
 }
 
-void test(double velocidad_inicial, double angulo, double perdida){
+double distancia_parabola(const double velocidad, const double sen_angulo, const double cos_angulo, const double posicion_inicial, const double restitucion){
+  return ((pow(restitucion, 3.0) * pow(velocidad, 2.0) * sen_angulo * cos_angulo) / (4.9)) + posicion_inicial; // 4.9 == g/2
+}
+
+void test(double velocidad_inicial, double angulo, double restitucion){
   double posicion_inicial = 0.15;
   char *resultadoA="La pelota baja por las escaleras sin saltarse ningun escalon", *resultadoB="La pelota no rebota dos veces en un mismo escalon", *resultadoC=resultadoB;
-  int escalon_primer_rebote = -1;
+  int escalon_doble_rebote = -1;
 
-  printf("TEST: velocidad inicial=%.2lf m/s; angulo=%.2lf grados; perdida=%.2lf%%\n", velocidad_inicial, angulo, perdida);
+  printf("TEST: velocidad inicial=%.2lf m/s; angulo=%.2lf grados; restitucion=%.1lf\n", velocidad_inicial, angulo, restitucion);
 
-  const double angulo_parabola = 90 - angulo, rad_angulo = (angulo_parabola * M_PI) / 180.0;
+  // Desparametrizamos las ecuaciones del tiro parabolico
+  // x(t) = x0 + v0*seno(ang)*t
+  // y(t) = y0 + v0*cos(ang)*t - 1/2 g t^2
+  //
+  // y(t) = y0 + t (v0*cos(ang) - g/2 t)
+  // y(t) = 0 => t = v0*cos(ang) / (g/2)
+  //
+  // x-x0 / v0*seno(ang) = t
+  // y(x) = cot(ang)*(x-x0) - g/2 * (x-x0 / v0*seno(ang))^2
+  // y(d) = cot(ang)*(d-x0) - (g/2) * (d-x0)^2 / (v*seno(ang))^2
 
-  // cuatroAC = 2 * 0.15 * g
-  // dosA = 2 * -1/2 g = -g
-  const double coseno = cos(rad_angulo), seno = sin(rad_angulo), dosA = -9.8, cuatroAC = 2.94, gravedad = 9.8;
+  // y(d) = 0 => cot(ang)*(d-x0) = (g/2) * (d-x0)^2 / (v*seno(ang))^2
+  // d = (v^2*seno(ang)*cos(ang) / (g/2)) + x0
+  //
+  // y(d) = -0.15 => cot(ang) = ((g/2) * (d-x0) / (v*seno(ang))^2) - 0.15/(d-x0)
+  // no puedo sacar d para un valor de y(d) distinto a 0.
 
-  const double velocidad_horizontal = velocidad_inicial * coseno;
-  double velocidad_vertical = velocidad_inicial * seno;
+  const double rad_angulo = (angulo * M_PI) / 180.0;
+  const double coseno = cos(rad_angulo), seno = sin(rad_angulo);
 
-  double t, siguiente_posicion;
+  int rebotes[8] = {0};
+
+  const double velocidad_horizontal = velocidad_inicial * seno;
+  double velocidad_vertical = velocidad_inicial * coseno * restitucion; 
+
+  // asumimos un primer rebote en 0.15m.
+  rebotes[0] = 1;
+
+  double t, distancia=0, siguiente_posicion, diff_escalon;
+  int escalon_previo=0;
+
+
+  // reviso si rebota en el mismo escalon.
   for (int escalon = 0; escalon < 8;) {
-    velocidad_vertical = velocidad_vertical * (1 - (perdida/100));
+    t = resolver_tiempo(escalon*(-ALTURA_ESCALON), velocidad_vertical);
 
-    // obtenemos el tiempo que falta para que rebote
-    t = resolventeSimplificada(velocidad_vertical, dosA, cuatroAC);
+    distancia = posicion_inicial + velocidad_horizontal*t;
 
-    // obtenemos la siguiente posicion
-    siguiente_posicion = posicion_inicial + velocidad_horizontal*t;
-
-    if (escalon_primer_rebote == -1 && siguiente_posicion < ANCHO_ESCALON) {
-      resultadoB = "La pelota rebota dos veces en un mismo escalon";
-      escalon_primer_rebote = escalon+1;
-    } else if (siguiente_posicion > 2*ANCHO_ESCALON) {
-      resultadoA = "La pelota se saltea un escalon";
+    if (distancia <= ANCHO_ESCALON) {
+      rebotes[escalon] += 1;
+      velocidad_vertical *= restitucion;
+      posicion_inicial = distancia;
+      escalon_previo = escalon;
+      // calculo en una nueva parabola.
+    } else {
+      // No rebota en este escalon, chequeamos el siguiente
+      escalon_previo = escalon;
+      escalon += distancia / ANCHO_ESCALON;
+      // movemos el origen al siguiente escalon
+      diff_escalon = (escalon - escalon_previo);
+      if (diff_escalon > 1) {
+      }
+      posicion_inicial = posicion_inicial - diff_escalon*ANCHO_ESCALON;
     }
+  }
 
-    escalon += (siguiente_posicion / ANCHO_ESCALON)+1;
-    printf("escalon: %d\n", escalon);
-    posicion_inicial = remainder(siguiente_posicion, ANCHO_ESCALON);
+  printf("rebotes: ");
+  for (int i=0; i<8; i++) {
+    printf("%d ", rebotes[i]);
+  }
+  printf("\n");
+
+  for (int i=0; i<8; i++) {
+    if (escalon_doble_rebote == -1 && rebotes[i] >= 2) {
+      escalon_doble_rebote = i;
+      resultadoB = "La pelota rebota dos veces sobre un mismo escalon";
+    }
+    if (rebotes[i] == 0) {
+      resultadoA = "La pelota se salta un escalon";
+    }
   }
 
   printf("a) %s\n", resultadoA);
   printf("b) %s\n", resultadoB);
-  if (escalon_primer_rebote != -1) {
+  if (escalon_doble_rebote != -1) {
     resultadoC = "La pelota rebota sobre el escalon";
-    printf("c) %s %d 2 veces\n\n", resultadoC, escalon_primer_rebote);
+    printf("c) %s %d al menos dos veces\n\n", resultadoC, escalon_doble_rebote+1);
   } else {
     printf("c) %s\n\n", resultadoC);
   }
@@ -110,7 +162,7 @@ int main(int argc, char *argv[])
       for (velocidad_inicial = 1.8; velocidad_inicial <= 3.0; velocidad_inicial += 0.6) {
         for (angulo = 18; angulo <= 26; angulo += 4) {
           for (perdida = 40; perdida <= 50; perdida += 10) {
-            test(velocidad_inicial, angulo, perdida);
+            test(velocidad_inicial, angulo, 1-(perdida/100));
           }
         }
       }
