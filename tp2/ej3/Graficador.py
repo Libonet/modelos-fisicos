3159644d8ec4b1fbeec27212d6534ed244c781a6
AGG-II <73410884+AGG-II@users.noreply.github.com>
2024-10-31 23:23:37 -0300

    Ahora está prolijo

--- tp2/ej3/Graficador.py
+++ tp2/ej3/Graficador.py
@@ -5,27 +5,47 @@ import matplotlib.pyplot as plt
 g = 9.8  # Gravedad
 r = 1.524  # Radio
 v0 = 3.5**2  # Velocidad inicial
+m = 0.45 # Masa
+energia_inicial = (1/2) * v0 * m + m * g * r
 
-# Rango de valores para x y y
-y_vals = np.linspace(0, np.pi / 2, 100)  # Valores de x en radianes de 0 a pi/2
-x_vals = np.linspace(0, 0.4, 100)  # Valores de y en el rango [0, 0.4]
+# Magnitud del roce
+def Fr(mu, theta):
+    return mu * g * m * np.cos(theta)
+
+# Altura cuando el bloque esta en el angulo theta
+def altura(theta):
+    return r * np.cos(theta)
+
+def potencial(theta):
+    return  m * g * altura(theta)
+
+def W_fnc(mu,theta):
+    return Fr(mu, theta) * theta * r
+
+def velocidad_cuadrado(mu, theta):
+    return (2/m) * (energia_inicial + W_fnc(mu,theta) - potencial(theta))
+
+
+radianes = np.linspace(0, np.pi / 2, 100)  # Valores de radianes de 0 a pi/2
+coeficientes = np.linspace(0, 0.4, 100)  # Valores de mu en el rango [0, 0.4]
 
 # Listas para almacenar los puntos que cumplen la igualdad
 x_points = []
 y_points = []
 
 # Comprobación de la igualdad
-for x in x_vals:
-    for y in y_vals:
-        lhs = ((v0 + 2 * g * (r - np.cos(x) * r) + 2 * y * g * np.cos(x) * x * r) ** 2) / np.cos(x)
-        rhs = r * g
-        if np.isclose(lhs, rhs, atol=1e-5):  # Tolerancia para la comparación
-            x_points.append(x)
-            y_points.append(y)
+for mu in coeficientes:
+    for theta in radianes:
+        division = (velocidad_cuadrado(mu, theta) / np.cos(theta)) - m * g 
+        print(division)
+        if np.isclose(division,0, atol=1e-1):  # Tolerancia para la comparación
+            x_points.append(mu)
+            y_points.append(theta)
 
 # Graficar los puntos que cumplen la igualdad
+print(len(x_points))
 plt.figure(figsize=(10, 6))
-plt.scatter(x_points, y_points, color='blue', s=10)
+plt.plot(x_points, y_points, color='b', label=r'$\theta(\mu_c)$')
 plt.xlabel('Coeficiente de rozamiento')
 plt.ylabel('Theta crítico')
 plt.title('Donde se desprende el bloque')
